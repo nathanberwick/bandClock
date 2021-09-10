0c2412f5983396ae435ec7f8487b314e7e78a402
bwk <nathan.berwick@iis.fraunhofer.de>
2021-09-10 17:54:38 +0200

    added CMAKE file for use in CLion. updated to V2 prototype. Currently untested.

--- bandClock.ino
+++ bandClock.ino
@@ -1,3 +1,4 @@
+#include "RTClib.h"
 #include <Stepper.h>
 #include <math.h>
 
@@ -10,43 +11,41 @@ const int stepsPerRevolution = 200;
 //calculated variables
 float stepLength = (spindleDiameter*PI)/stepsPerRevolution;
 float stepsPerBand = timingBeltLength/stepLength;
-float delayBetweenSteps = 86400000/stepsPerBand;
+float stepsPerSecond = stepsPerBand/86400; //24*60*60, or 24hrs into seconds.
 double modfreturn;
-float decimalDelay = modf(delayBetweenSteps, &modfreturn);
+float decimalDelay = modf(stepsPerSecond, &modfreturn);
 int counterForRound = round(1/decimalDelay);
-int counter = 1;
-int intStepDelay = delayBetweenSteps;
-//TODO: implement a counter system to add all the rounded bits together to make a "every x times, +1"
+int counter = 1; //TODO: figure out whether this should be zero at startup
+int previousSecond = 0;
 
 //system variables
+RTC_DS3231 rtc;
 Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
 const int speedPin = 12;
 bool buttonState = false;
 
 void setup()
 {
+  rtc.begin();
   pinMode(speedPin,INPUT);
   pinMode(13, OUTPUT);
   myStepper.setSpeed(fullSpeedForReset);
 }
   
-void loop()
-{
-    myStepper.step(1);
-    for (int j = 0; j < intStepDelay; j++)
-    {
-      buttonState = digitalRead(speedPin);
-      if (!buttonState)
-      {
-        delay(1);
-      }
-      counter++;
-      if (counter == counterForRound)
-      {
-        digitalWrite(13, HIGH);
-        delay(1);
-        digitalWrite(13,LOW);
-        counter = 1;
-      }
+void loop() {
+  DateTime currentTime = rtc.now();
+  buttonState = digitalRead(speedPin);
+
+  if (previousSecond != currentTime.second() && !buttonState) {
+    previousSecond = currentTime.second();
+    myStepper.step(decimalDelay);
+    counter++;
+    if (counter == counterForRound) {
+      counter = 1;
+      myStepper.step(1);
+    }
+  } else if (buttonState)
+  {
+    myStepper.step(1); //if button pressed, keep stepping at full speed.
   }
 }
